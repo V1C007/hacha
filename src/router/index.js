c88aa8ba5f3d867c3a86d3be5d065cd1d4e578c4
victor <20151713@aguascalientes.tecnm.mx>
2023-12-09 23:11:03 -0600

    componentes a usar

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Catalogo from '../components/Catalogo.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/Catalogo',
+    name: 'Catalogo',
+    component: Catalogo
   }
+
 ]
 
 const router = createRouter({
