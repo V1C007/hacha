5969b2de4815c0f20de79c1ab0fbb318855803c0
victor <20151713@aguascalientes.tecnm.mx>
2023-12-11 14:03:58 -0600

    vistas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,195 @@
-<!-- HomeView.vue -->
-
 <template>
+  <navbar></navbar>
+
+  <div class="home">
     <div>
-      <h1>Bienvenidos a Mi Página de Inicio</h1>
-      <p>Esta es la página de inicio de mi aplicación.</p>
+      <div class="max-w-screen-md mx-auto">
+        <div class="mb-4 sm:flex sm:flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+          <div
+            class="w-full sm:w-2/5 sm:shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+            <img src="@/assets/GIB-Card1.jpg" alt="imagen de hacha" title="imagen producto hacha"
+              class="w-full h-48 object-cover sm:h-full" />
+          </div>
+          <div class="p-6">
+            <h6
+              class="mb-2 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+              Interesante
+            </h6>
+            <h2
+              class="mb-2 block font-sans text-xl sm:text-2xl font-semibold sm:font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">
+              HACHA
+            </h2>
+            <p class="mb-4 block font-sans text-sm sm:text-base font-normal leading-relaxed text-gray-700 antialiased">
+              Lo mejor
+              de lo mejor.
+            </p>
+            <a class="inline-block" href="#" title="Enlace para más información">
+
+              <router-link :to="{ name: 'Producto', params: { id: 1 } }"
+                class="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40"
+                title="Producto">
+                Comprar YA
+              </router-link>
+            </a>
+          </div>
+        </div>
+
+        <div class="w-full sm:flex sm:flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+          <div
+            class="w-full sm:w-2/5 sm:shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+            <img src="@/assets/GIB-Card2.jpg" alt="imagen de hacha" title="producto hacha"
+              class="w-full h-48 object-cover sm:h-full" />
+          </div>
+          <div class="p-6">
+            <h6
+              class="mb-2 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+              Interesante
+            </h6>
+            <h4
+              class="mb-2 block font-sans text-xl sm:text-2xl font-semibold sm:font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">
+              Todo lo que necesitas
+            </h4>
+            <p class="mb-4 block font-sans text-sm sm:text-base font-normal leading-relaxed text-gray-700 antialiased">
+              No necesitaras otra hacha en toda
+              tu vida.
+            </p>
+            <a class="inline-block" href="#" title="Enlace para más información ">
+
+              <router-link :to="{ name: 'Producto', params: { id: 2 } }"
+                class="select-none rounded-lg bg-black py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40"
+                title="Producto">
+                Comprar YA
+              </router-link>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HomeView',
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilos específicos para este componente, si es necesario */
-  </style>
-  
+
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+import navbar from "../components/navbar.vue"
+
+export default {
+  components: {
+    navbar
+  },
+  data() {
+    return {
+      x: ref(0)
+    };
+  },
+  methods: {
+    onMousemoveA(e) {
+      this.x = e.clientX;
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*Estilos para la animacion del banner*/
+
+.hiddenBG {
+  opacity: 0;
+  background-image: url(@/assets/GIB-HomeBanner.jpg);
+  background-size: 100% auto;
+  background-position: center;
+  background-repeat: no-repeat;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border: 0px solid rgb(0, 0, 0);
+  margin: 0 auto;
+  max-width: 100% auto;
+  transition: opacity 5s ease;
+}
+
+.visibleBG {
+  opacity: 1;
+  background-image: url(@/assets/GIB-HomeBanner.jpg);
+  background-size: 100% auto;
+  background-position: center;
+  background-repeat: no-repeat;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border: 0px solid rgb(0, 0, 0);
+  margin: 0 auto;
+  max-width: 100% auto;
+  transition: opacity 5s ease;
+}
+
+.hiddenA-text {
+  color: hsla(0, 0%, 0%, 0);
+  text-align: center;
+  font-weight: bold;
+  font-size: 1px;
+  transition: color 2s ease, font-size 5s ease;
+}
+
+.hiddenB-text {
+  color: hsla(0, 0%, 0%, 0);
+  text-align: right;
+  font-size: 350px;
+  transition: color 2s ease, font-size 5s ease;
+}
+
+.hiddenC-text {
+  color: hsla(0, 0%, 0%, 0);
+  text-align: right;
+  font-size: 110px;
+  transition: color 2s ease, font-size 5s ease;
+}
+
+.hiddenD-text {
+  color: hsla(0, 0%, 0%, 0);
+  text-align: right;
+  font-size: 20px;
+  transition: color 2s ease, font-size 5s ease;
+}
+
+.visible-text {
+  color: hsl(0, 0%, 100%);
+  font-weight: bold;
+  font-size: 50px;
+  transition: color 3s ease, font-size 5s ease;
+  text-align: center;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
+}
+
+.hiddenD-button {
+  border: 0px;
+  background: transparent;
+  font-size: 0px;
+  color: transparent;
+}
+
+
+.visible-button {
+  display: block;
+  padding: 15px 30px;
+  border: 2px solid #ffffff;
+  background: transparent;
+  color: #ffffff;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 0 auto;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.8);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.8);
+  transition: all 3s ease;
+}
+
+.visible-button:hover {
+  background: rgba(255, 255, 255, 0.473);
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.4);
+  transform: scale(1.05);
+  transition: all 0.3s ease;
+}
+</style>
---
+++ src/views/Perfil.vue
@@ -0,0 +1,36 @@
+<template>
+    <navbar></navbar>
+    <div class="m-5 sm:x-auto lg:m-5  ">
+        <div class="w-full text-center">
+            <h1 class="text-3xl">Perfil de usuario</h1>
+        </div>
+        <div class="sm:w-full lg:w-1/3 mx-auto mt-5">
+            <label for="nombre">Nombre</label>
+            <input type="text" id="nombre" v-model="usuario.Nombre" class="mt-3 block w-full p-2 border border-black">
+            <label for="Correo">Correo</label>
+            <input type="text" id="Correo" v-model="usuario.Correo" class="mt-3 block w-full p-2 border border-black">
+            <label for="pass"> Password</label>
+            <input type="password" id="pass" v-model="usuario.Password" class="mt-3 block w-full p-2 border border-black">
+        </div>
+    </div>
+</template>
+
+<script>
+import navbar from "../components/navbar.vue"
+export default {
+    name: 'ViewUser',
+    components: {
+        navbar
+    },
+    data() {
+        return {
+            usuario: {
+                "id": 1,
+                "Nombre": "Antonio Lopez Lopez",
+                "Correo": "AntLopez@hotmail.com",
+                "Password": "123456789"
+            }
+        }
+    }
+}
+</script>
