4d33aa777f7af5d5855828fac77fe6552d689a29
victor <20151713@aguascalientes.tecnm.mx>
2023-12-11 14:36:32 -0600

    cambio en index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Catalogo from '@/components/Catalogo.vue'
+import Producto from "../components/Producto.vue"
+import CatalogoComponent from '../components/Catalogo.vue'
+import PayPalView from "../components/PayPalView.vue"
+import reviewForm from "../components/reviewForm.vue"
+import UserAdmin from "../views/UserAdmin"
+import Admin from "../views/Admin"
+import OrdenesCompra from "../components/OrdenesCompra.vue"
+import LoginComponent from "../components/LoginComponent.vue"
+import SignUp from "../components/SignUp.vue"
+import LoginUser from '../components/LoginUser.vue'
+import WishlistComponent from "../components/WishlistComponent.vue"
+import CartComponent from "../components/CartComponent.vue"
+import Perfil from "../views/Perfil"
 
 const routes = [
   {
@@ -9,9 +21,76 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/atalogo',
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/OrdenesCompra',
+    name: 'OrdenesCompra',
+    component: OrdenesCompra
+  },
+  {
+    path: '/Catalogo',
     name: 'Catalogo',
-    component: Catalogo
+    component: CatalogoComponent
+  },
+  {
+    path: '/Producto/:id',
+    name: 'Producto',
+    component: Producto
+  },
+  {
+    path: '/Perfil',
+    name: 'Perfil',
+    component: Perfil
+  },
+  {
+    path: '/Admin',
+    name: 'Admin',
+    component: Admin
+  },
+
+  {
+    path: '/UserAdmin',
+    name: 'UserAdmin',
+    component: UserAdmin
+  },
+  {
+    path: '/reviewForm',
+    name: 'reviewForm',
+    component: reviewForm
+  },
+  {
+    path: '/LoginComponent',
+    name: 'LoginComponent',
+    component: LoginComponent
+  },
+  {
+    path: '/LoginUser',
+    name: 'LoginUser',
+    component: LoginUser
+  },
+  {
+    path: '/SignUp',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/Wishlist',
+    name: 'Wishlist',
+    component: WishlistComponent
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    component: CartComponent
+  },
+  {
+    path: '/paypal',
+    name: 'paypal',
+    component: PayPalView,
+    props: true
   }
 
 ]
