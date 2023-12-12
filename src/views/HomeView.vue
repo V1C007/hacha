74eb202f5ec315f64e7c585cc2ea9d54cb1c267c
victor <20151713@aguascalientes.tecnm.mx>
2023-12-11 22:59:35 -0600

    home2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,31 @@
 <template>
+  <!-- Componente de barra de navegación -->
   <navbar></navbar>
 
   <div class="home bg-gray-100">
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-4">
-      <div v-for="producto in productos.slice(0, 4)" :key="producto.id"
+    <!-- Grid para mostrar productos, 1 columna en móviles y 2 en pantallas más grandes -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 p-4">
+      <!-- Bucle para mostrar cada producto (limitado a 4 productos) -->
+      <div v-for="(producto, index) in productos.slice(0, 4)" :key="index"
         class="rounded-xl bg-white bg-clip-border text-gray-700 shadow-md overflow-hidden">
+        <!-- Imagen del producto -->
         <img :src="require(`@/assets/assets/${producto.imagen}`)" :alt="producto.nombre" :title="producto.nombre"
           class="w-full h-48 object-cover" />
+        <!-- Detalles del producto -->
         <div class="p-6">
-          <h6 class="mb-2 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-green-500 antialiased">
-            ¡Destacado!
+          <!-- Etiqueta "Interesante" -->
+          <h6 class="mb-2 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+            Interesante
           </h6>
+          <!-- Nombre del producto -->
           <h2 class="mb-2 block font-sans text-xl sm:text-2xl font-semibold sm:font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">
             {{ producto.nombre }}
           </h2>
+          <!-- Descripción del producto -->
           <p class="mb-4 block font-sans text-sm sm:text-base font-normal leading-relaxed text-gray-700 antialiased">
             {{ producto.descripcion }}
           </p>
+          <!-- Enlace para ir a la página del producto -->
           <router-link :to="{ name: 'Producto', params: { id: producto.id }}"
             class="select-none rounded-lg bg-green-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md transition-all hover:bg-green-600">
             Comprar YA
@@ -39,11 +48,13 @@ export default {
   data() {
     return {
       x: ref(0),
+      // Lista de productos cargada desde el archivo JSON
       productos: products.productos || []
     };
   },
   methods: {
     onMousemoveA(e) {
+      // Función para manejar eventos de movimiento del mouse
       this.x = e.clientX;
     }
   }
