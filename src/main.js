18bb1493238cb0ca180c1dbfa99328446984750e
victor <20151713@aguascalientes.tecnm.mx>
2023-12-11 19:39:18 -0600

    cambio en diseño producto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import '@/assets/assets/main.css';
 
-createApp(App).use(router).mount('#app');
+library.add(faHeart);
+
+createApp(App)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
